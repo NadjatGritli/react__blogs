0662b84b5befda9689ed9e4c44670cf35bdc9d8a
nadjatgritli <50056310+NadjatGritli@users.noreply.github.com>
2024-05-13 14:47:38 +0100

    add some inforemations to footer part

--- src/Pages/Store/storeLayout/footer.js
+++ src/Pages/Store/storeLayout/footer.js
@@ -1,4 +1,28 @@
 const StoreFooter = ()=>{
-    <div className="footer"></div>
+    <div className="footer">
+    <div className="row">
+        <div className="col-3">
+            <div className="footer_logo"></div>
+            <div className="footer_description"></div>
+            <div className="footer_phone"></div>
+            <div className="footer_social_media"></div>
+        </div>
+        <div className="footer_links"></div>
+        <div className="col-3">
+            <a href="#" className="footerlink">
+                About us
+            </a>
+            <a href="#" className="footerlink">
+                Contact us
+            </a>
+            <a href="#" className="footerlink">
+                Terms of use
+            </a>
+            <a href="#" className="footerlink">
+                Privacy
+            </a>
+        </div>
+    </div>
+    </div>
 }
 export default StoreFooter
